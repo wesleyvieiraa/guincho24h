e5109a1aee3ac88c4ff48dd8c91f18bb7330fe59
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2023-03-10 21:53:26 -0300

    Feat: Inserindo ambiente de produção

---
+++ front/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://guincho24h.onrender.com/api/v1',
+  serverBaseUrl: 'https://guincho24h.onrender.com',
+  cacheLifeTime: 60 * 1000,
+};
