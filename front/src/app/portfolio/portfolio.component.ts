7fd78028dcbb899f9406ab0b8020182ff498b8f9
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2023-03-21 20:08:38 -0300

    Fix: Manutenção no portfolio

--- front/src/app/portfolio/portfolio.component.ts
+++ front/src/app/portfolio/portfolio.component.ts
@@ -1,3 +1,4 @@
+import { CallService } from './../services/call.service';
 import { PortfolioService } from './portfolio.service';
 import { Company } from './../_models/company.model';
 import { Component, OnInit } from '@angular/core';
@@ -6,7 +7,8 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  providers: [CallService]
 })
 export class PortfolioComponent implements OnInit {
 
@@ -29,6 +31,7 @@ export class PortfolioComponent implements OnInit {
   constructor(
     private portfolioService: PortfolioService,
     private route: ActivatedRoute,
+    private callService: CallService
   ) { }
 
   ngOnInit(): void {
@@ -56,20 +59,28 @@ export class PortfolioComponent implements OnInit {
     this.loading = true;
     this.company = company;
 
-    this.portfolioService.sendWarningCompany(company).subscribe(
-      response => {
-        this.loading = false;
-      }, 
-      fail => {
-        this.loading = false;
-      }
-    );    
+    // this.portfolioService.sendWarningCompany(company).subscribe(
+    //   response => {
+    //     this.loading = false;
+    //   }, 
+    //   fail => {
+    //     this.loading = false;
+    //   }
+    // );    
   }
 
   generateWhatsAppLink(number: string) {
     const message = "Olá! Preciso de um guincho";
     const url = `https://wa.me/${number}?text=${encodeURIComponent(message)}`;
     window.open(url);
+  }
+
+  makeCall(phoneNumber: string) {
+    this.callService.makeCall(phoneNumber);
+  }
+
+  isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
   
 }
